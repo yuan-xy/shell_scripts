b8c02c8551d11ce2ff6c3b89a69c53ec8d3852b6
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-11-22 17:30:50 +0800

    enviroment customize

--- ali/setup1.sh
+++ ali/setup1.sh
@@ -2,7 +2,7 @@ apt-get update
 apt-get -y install systemtap dstat w3m iptables-persistent
 apt-get -y install elfutils
 apt-get -y install linux-headers-3.2.0-29-generic  gcc libcap-dev build-essential libncurses5-dev git libexpat1-dev zlib1g-dev libssl-dev
-apt-get -y install htop nmap rpl ruby1.9.3 expect redis-server nginx-full
+apt-get -y install htop nmap rpl libreadline-dev ruby1.9.3 expect nginx-full imagemagick
 #/usr/sbin/update-rc.d -f nginx defaults 
 
 nohup ./setup11.sh &
--- ali/setup4.sh
+++ ali/setup4.sh
@@ -5,4 +5,21 @@ echo "input the passwd of new user lian"
 passwd lian
 
 read -p "machine nickname:" nickname
-echo "export PS1='\[\e[0;36m\]\u-${nickname}\[\e[m\]\[\e[0;34m\]\w\\$ \[\e[m\]'" >> /etc/profile
+echo "export PS1='\[\e[0;35m\]\u-${nickname}\[\e[m\]\[\e[0;34m\]\w\\$ \[\e[m\]'" >> /etc/profile
+echo "export PS1='\[\e[0;35m\]\u-${nickname}\[\e[m\]\[\e[0;34m\]\w\\$ \[\e[m\]'" >> /root/.bash_profile
+echo "export PS1='\[\e[0;35m\]\u-${nickname}\[\e[m\]\[\e[0;34m\]\w\\$ \[\e[m\]'" >> /mnt/.bash_profile
+
+echo "export RAILS_ENV='production'" >> /mnt/.bash_profile
+
+cd /mnt && git clone https://github.com/yuanxinyu/shell_scripts.git
+chown -R lian:lian /mnt/shell_scripts
+echo "export PATH=/mnt/shell_scripts:$PATH" >> /etc/profile
+
+echo "alias lg='ls -G --color'" >> /etc/profile
+echo "alias ll='ls -l --color'" >> /etc/profile
+echo "alias gst='git status'" >> /etc/profile
+echo "alias gca='git commit -a'" >> /etc/profile
+echo "alias gcam='git commit -a -m '" >> /etc/profile
+echo "alias erl='rlwrap -a  -A erl'" >> /etc/profile
+
+
