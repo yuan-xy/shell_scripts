5c9681db470b99c1bc7658f21d75c8ed1ae5660e
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-03-20 10:45:32 +0800

    m

--- ali/setup1.sh
+++ ali/setup1.sh
@@ -1,9 +1,13 @@
 apt-get update
 apt-get -y install systemtap dstat w3m iptables-persistent
-apt-get -y install elfutils
+apt-get -y install elfutils ascii apache2-utils
 apt-get -y install linux-headers-3.2.0-29-generic  gcc libcap-dev build-essential libncurses5-dev git libexpat1-dev zlib1g-dev libssl-dev
 apt-get -y install htop nmap rpl libreadline-dev ruby1.9.3 expect nginx-full imagemagick
 #/usr/sbin/update-rc.d -f nginx defaults 
+
+#rpl "1024" "102400"  /usr/include/x86_64-linux-gnu/bits/typesizes.h
+#rpl "1024" "102400"  /usr/include/linux/posix_types.h
+#apt-get -y install httperf
 
 nohup ./setup11.sh &
 
--- ali/setup4.sh
+++ ali/setup4.sh
@@ -9,6 +9,9 @@ echo "export PS1='\[\e[0;35m\]\u-${nickname}\[\e[m\]\[\e[0;34m\]\w\\$ \[\e[m\]'"
 echo "export PS1='\[\e[0;35m\]\u-${nickname}\[\e[m\]\[\e[0;34m\]\w\\$ \[\e[m\]'" >> /root/.bash_profile
 echo "export PS1='\[\e[0;35m\]\u-${nickname}\[\e[m\]\[\e[0;34m\]\w\\$ \[\e[m\]'" >> /mnt/.bash_profile
 
+git config --global user.email "${nickname}@dface.cn"
+git config --global user.name ${nickname}
+
 echo "export RAILS_ENV='production'" >> /mnt/.bash_profile
 
 cd /mnt && git clone https://github.com/yuanxinyu/shell_scripts.git
@@ -21,5 +24,7 @@ echo "alias gst='git status'" >> /etc/profile
 echo "alias gca='git commit -a'" >> /etc/profile
 echo "alias gcam='git commit -a -m '" >> /etc/profile
 echo "alias erl='rlwrap -a  -A erl'" >> /etc/profile
+echo "alias dstat='dstat -cdlmnpsy'" >>  /etc/profile
+
 
 
