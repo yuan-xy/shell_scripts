14390e6837e539bb6fc03e5d438039cad55ff9f8
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-04-30 15:48:03 +0800

    riak diag建议的系统配置

--- ali/setup5.sh
+++ ali/setup5.sh
@@ -4,6 +4,15 @@ echo "*  hard  nofile  999999" >> /etc/security/limits.conf
 cat /proc/sys/fs/file-max 
 
 echo "net.ipv4.ip_local_port_range = 10240 61000" >> /etc/sysctl.conf
+echo "vm.swappiness = 0" >> /etc/sysctl.conf
+echo "net.core.wmem_default = 8388608" >> /etc/sysctl.conf
+echo "net.core.rmem_default = 8388608" >> /etc/sysctl.conf
+echo "net.core.wmem_max = 8388608" >> /etc/sysctl.conf
+echo "net.core.rmem_max = 8388608" >> /etc/sysctl.conf
+echo "net.core.netdev_max_backlog  = 10000" >> /etc/sysctl.conf
+echo "net.core.somaxconn = 4000" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_syn_backlog = 40000" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_fin_timeout = 15" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_tw_reuse = 1" >> /etc/sysctl.conf
 
 sysctl -p
-
