7fab9dd29176d454aaa1baf8b6d75128b44c1349
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-04-30 15:47:37 +0800

    升级到ext4

--- ali/setup3.sh
+++ ali/setup3.sh
@@ -1,5 +1,5 @@
-mkfs.ext3 /dev/xvdb1 
-echo '/dev/xvdb1  /mnt ext3    defaults    0  0' >> /etc/fstab
+mkfs.ext4 /dev/xvdb1 
+echo '/dev/xvdb1  /mnt ext4    defaults,noatime,nodiratime 0 2' >> /etc/fstab
 cat /etc/fstab
 mount -a
 df -h
