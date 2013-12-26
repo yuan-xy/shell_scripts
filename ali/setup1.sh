3ec4fa7f8c988611edb1af14aea566951975749a
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-12-26 15:29:02 +0800

    m

--- ali/setup1.sh
+++ ali/setup1.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get -y install systemtap dstat w3m iptables-persistent
 apt-get -y install elfutils ascii apache2-utils
-apt-get -y install linux-headers-3.2.0-29-generic  gcc libcap-dev build-essential libncurses5-dev git libexpat1-dev zlib1g-dev libssl-dev
+apt-get -y install linux-headers-3.2.0-29-generic  gcc libcap-dev build-essential libncurses5-dev git libexpat1-dev zlib1g-dev libssl-dev libsasl2-dev
 apt-get -y install htop nmap rpl libreadline-dev ruby1.9.3 expect nginx-full imagemagick
 #/usr/sbin/update-rc.d -f nginx defaults 
 
