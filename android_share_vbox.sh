528bc497b236f5fd9576f2cc8b56d5c8695f59f3
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2014-06-02 22:29:28 +0800

    让微云／百度云等APP能够同步电脑硬盘中存在的大量文件

--- android_share_vbox.sh
+++ android_share_vbox.sh
@@ -1,8 +1,9 @@
-#让微云等APP能够同步电脑硬盘中存在的大量文件
+#让微云／百度云等APP能够同步电脑硬盘中存在的大量文件
 #1. 在virtualbox中设置文件夹共享，比如名字为baby
 #2. 启动Genymotion安卓模拟器
 adb shell
 su
 umount /mnt/shared/baby
 exec /system/bin/androVM-vbox-sf /storage/emulated/legacy/DCIM/
+#3. 在微云／百度云等APP中使用照片／文件夹同步即可。
 
