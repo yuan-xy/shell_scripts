7215f49fb771151053ce038327df16b74b4215c2
yuan xinyu <yuanxinyu.hangzhou@gmail.com>
2019-05-24 09:13:24 +0800

    基于iptables实现流量转发

---
+++ ipforward.sh
@@ -0,0 +1,11 @@
+# ip流量转发
+#vi /etc/sysctl.conf
+#把net.ipv4.ip_forward= 0修改成net.ipv4.ip_forward= 1
+#保存后执行sysctl -p让修改生效
+
+
+iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination 目标服务器ip:443
+iptables -t nat -A PREROUTING -p udp --dport 443 -j DNAT --to-destination 目标服务器ip:443
+iptables -t nat -A POSTROUTING -p tcp -d 目标服务器ip --dport 443 -j SNAT --to-source 中转服务器ip
+iptables -t nat -A POSTROUTING -p udp -d 目标服务器ip --dport 443 -j SNAT --to-source 中转服务器ip
+
