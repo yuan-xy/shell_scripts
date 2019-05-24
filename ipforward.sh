1cf67c167228bbc87aa2113ae3af56a623f4853f
yuan xinyu <yuanxinyu.hangzhou@gmail.com>
2019-05-24 09:20:57 +0800

    m

--- ipforward.sh
+++ ipforward.sh
@@ -1,5 +1,7 @@
 # ip流量转发
-#vi /etc/sysctl.conf
+
+# 在中转服务器上执行下面的命令
+vi /etc/sysctl.conf
 #把net.ipv4.ip_forward= 0修改成net.ipv4.ip_forward= 1
 #保存后执行sysctl -p让修改生效
 
@@ -9,3 +11,6 @@ iptables -t nat -A PREROUTING -p udp --dport 443 -j DNAT --to-destination 目标
 iptables -t nat -A POSTROUTING -p tcp -d 目标服务器ip --dport 443 -j SNAT --to-source 中转服务器ip
 iptables -t nat -A POSTROUTING -p udp -d 目标服务器ip --dport 443 -j SNAT --to-source 中转服务器ip
 
+#别忘了保存规则和重启iptables
+service iptables save
+service iptables restart
